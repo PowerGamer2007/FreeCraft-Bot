a494fce22a5e5f1aa2d59093a3c42de5c1017fc2
PowerGamer2007 <59776674+PowerGamer2007@users.noreply.github.com>
2020-05-16 11:17:56 +0200

    FreeCraft bot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const { prefix, token } = require('./config.json')
 const fs = require("fs");
 Discord.commands = new Discord.Collection();
